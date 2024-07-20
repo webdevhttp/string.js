4074cb6b1f3ca0a7cf995358416648111bb41df8
Tasha SEii <liamluke2012@icloud.com>
2024-07-20 13:42:12 +0200

    Add files via upload
    adding string as js

---
+++ script.js
@@ -0,0 +1,8 @@
+var username;
+var message;
+username='Molly';
+message="Houdy";
+var elName=document.getElementById('name');
+elName.textContent=username;
+var elNote=document.getElementById ('note');
+elNote.textContent=message;
